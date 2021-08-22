880cca2d3132f63e2a65bfa295a3677dff101dcf
vincentmllr <vincent-mueller-97@gmx.de>
2021-08-22 19:54:26 +0200

    -Smaller fixes

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+console.log("Skript gestartet.");
+
 const title = document.getElementById("title");
+
+console.log("Skript funktioniert.", title);
 
 // title.style.animation.bold;
 
