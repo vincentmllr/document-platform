d92de821855ee6a66bc9b4c824daa78e05808f80
upvav <adrian.wimmer@student.kit.edu>
2021-09-03 19:04:15 +0200

    Update WissenschaftlicheArbeit.sol

--- Project/app/smart_contracts/WissenschaftlicheArbeit.sol
+++ Project/app/smart_contracts/WissenschaftlicheArbeit.sol
@@ -1,1 +1,17 @@
+pragma solidity 0.5.1;
 
+  
+contract WissenschaftlicheArbeit {
+    
+    string public titel;
+    string public autor;
+    
+    constructor(string memory _titel, string memory _autor)public{
+        
+        titel =_titel;
+        autor = _autor;
+    }
+   
+
+    
+}
