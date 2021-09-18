82f35d02ebbc45036adc93e2fd7caca605aedbee
upvav <adrian.wimmer@student.kit.edu>
2021-09-18 17:02:52 +0200

    Update scientificWork.sol

--- Project/Test/app/smart_contracts/scientificWork.sol
+++ Project/Test/app/smart_contracts/scientificWork.sol
@@ -1,1 +1,47 @@
+pragma solidity 0.5.1;
 
+contract scientificWork {
+    string title;
+    string author;
+    string path;
+    string hashcode;
+    bool isSignt = false;
+    address changeExaminer; 
+   
+   
+    
+    constructor(string memory _title, string memory _author, string memory _path, string memory _hashcode, address _changeExaminer)public{
+        
+        title =_title;
+        author = _author;
+        path = _path;
+        hashcode = _hashcode;
+        changeExaminer = _changeExaminer;
+    }
+      modifier onlyExaminer(){
+        require(msg.sender == changeExaminer);
+        _;
+    }
+    
+        function signScientificWork() public onlyExaminer {
+        isSignt = true;
+        
+    }
+  
+    function getPath()public view returns(string memory){
+        return path;
+    }
+    
+    function changeTitle(string memory _title) public{
+        title = _title;
+        
+    }
+    
+    function changeAuthor(string memory _author) public{
+        author = _author;
+        
+    }
+    
+
+    
+}
