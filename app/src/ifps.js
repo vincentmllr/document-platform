058e85857f16cc2c9d099f1b6b70da004d0a151b
ujodc <ujodc@student.kit.edu>
2021-09-22 15:12:02 +0200

    Anbidnung Blockchain

--- app/src/ifps.js
+++ app/src/ifps.js
@@ -9,8 +9,32 @@ export async function uploadFile(file){
     return added.path;
 }
 
-export async function downloadFiles(path){
-    for await (const chunk of client.cat(path)) {
-        console.log(chunk);
-      }
+export async function downloadFiles(pathList){
+    
+    var filesAsUint8 = [];
+    var chunks =[];
+    for (var i = 0; i < pathList.length; i++) {
+        for await (const chunk of client.get(pathList[i])) {
+            chunks.push(chunk)
+          }
+          
+        // Get the total length of all arrays.
+        let length = 0;
+        chunks.forEach(item => {
+            length += item.length;
+        });
+
+        // Create a new array with total length and merge all source arrays.
+        let mergedArray = new Uint8Array(length);
+        let offset = 0;
+        chunks.forEach(item => {
+            mergedArray.set(item, offset);
+            offset += item.length;
+        });
+
+        // Should print an array with length 90788 (5x 16384 + 8868 your source arrays)
+        filesAsUint8.push(mergedArray);
+    }
+
+    return filesAsUint8;
 }
